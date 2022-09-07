316e3ef5985c92b6341f1d6c184f4f67adfe855b
mgntpk <mgntpkgit@gmail.com>
2022-09-07 19:13:27 +0900

    sdkversion30~32対応に変更など。

--- app/src/main/java/com/twitter/mgntpk/dice/MainActivity.kt
+++ app/src/main/java/com/twitter/mgntpk/dice/MainActivity.kt
@@ -1,31 +1,30 @@
 package com.twitter.mgntpk.dice
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.MotionEvent.*
+import android.view.MotionEvent.ACTION_DOWN
+import androidx.appcompat.app.AppCompatActivity
 import com.twitter.mgntpk.dice.databinding.ActivityMainBinding
 import java.security.SecureRandom
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        fun diceRoll(): Int = SecureRandom().nextInt(6)+1
+        fun diceRoll(): Int = SecureRandom().nextInt(6) + 1
 
         "${diceRoll()}".also { binding.textView.text = it }
 
-        binding.textView.setOnTouchListener { view, event ->
-            if (event.action == ACTION_DOWN){
+        binding.textView.setOnTouchListener { _, event ->
+            if (event.action == ACTION_DOWN) {
                 "${diceRoll()}".also { binding.textView.text = it }
             }
             true
         }
     }
 }
-
-
-
